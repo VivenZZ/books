7ad08a3e62ce2babe35f5012235e95aa9b08c634
VivenZ <17706500512@163.com>
2019-11-02 10:12:27 +0800

    (node:12636) DeprecationWarning: current Server Discovery and Monitoring engine is deprecated, and will be removed  in a future version. To use the new Server Discover and Monitoring engine, pass option { useUnifiedTopology: true  } to the MongoClient constructor. 警告报错

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/books', { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/books', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 mongoose.connection.once('open', function (callback) {
     console.log("数据库成功连接");
